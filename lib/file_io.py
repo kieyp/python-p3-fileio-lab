e2df257f0d7555f09917eb492eafda0fc05b54ff
Boniface <cheruiyotbony@gmail.com>
2024-01-26 09:34:50 +0300

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name , file_content):
+    with open( f'{file_name}.txt', 'w') as file:
+        file.write(file_content)
+
+# Example usage:
+# write_file(file_name='example.txt', file_content='This is the content.')
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as file:
+        file.write(append_content)
+
+# Example usage:
+# append_file(file_name='example.txt', append_content='This is additional content.')
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as file:
+     return file .read()
+
+# Example usage:
+# content = read_file(file_name='example.txt')
+# print(content)
+
